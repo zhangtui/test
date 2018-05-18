097e640867ebbf934bbcbfcdcb5cd5f37c3cd273
zhangtui <weirui.zhang@timanetworks.com>
2018-05-18 10:43:09 +0800

    创建项目

---
+++ src/main/java/interfacerestAssured/MathTool.java
@@ -0,0 +1,14 @@
+package interfacerestAssured;
+
+public class MathTool {
+	//通过程序来求整数的绝对值
+	
+		public static int abs(int a) {
+			//通过控制结构：分支、循环
+			if(a<0){
+				return -a;
+			}else {
+				return a;
+			}
+		}
+}
---
+++ src/test/java/interfacerestAssured/TestAppLogin.java
@@ -0,0 +1,14 @@
+package interfacerestAssured;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import org.junit.Test;
+public class TestAppLogin {
+	@Test
+	public void TestLogin(){
+		given();
+	}
+	
+
+}
